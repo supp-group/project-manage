e1447b22dad23502c129576a861c2732865c2873
HindAlmorshed <81118373+HindAlmorshed@users.noreply.github.com>
2024-03-05 11:55:01 +0200

    route

--- resources/views/admin/member/details.blade.php
+++ resources/views/admin/member/details.blade.php
@@ -20,7 +20,7 @@
 				<div class="breadcrumb-header justify-content-between">
 					<div class="my-auto">
 						<div class="d-flex">
-							<h4 class="content-title mb-0 my-auto">الأعضاء</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ تعديل عضو</span>
+							<h4 class="content-title mb-0 my-auto">الأعضاء</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ تفاصيل العضو</span>
 						</div>
 					</div>
 				</div>
@@ -33,8 +33,9 @@
 					<div class="col-lg-12 col-md-12">
 						<div class="card">
 							<div class="card-body">
-								<form action="{{ route('getDetails', $member->id) }}" method="get" autocomplete="off">
+								<form action="{{ url('details', [$member->id]) }}" method="get" autocomplete="off">
 									{{ csrf_field() }}
+                                    @method('get')
 
                                     <div class="row">
 										<div class="col">
