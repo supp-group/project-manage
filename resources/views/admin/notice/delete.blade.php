03fe8b3638e415c65a0042a2415a63ae55c5c25d
HindAlmorshed <81118373+HindAlmorshed@users.noreply.github.com>
2024-03-09 12:34:59 +0200

    e

--- resources/views/admin/notice/delete.blade.php
+++ resources/views/admin/notice/delete.blade.php
@@ -52,9 +52,9 @@
 							</div>
 
 							<div class="dash-widget-info text-left">
-								{{-- <a class="btn btn-sm btn-info" href="{{ route('notice.deleteDetails', $member->IDTeam) }}" style="font-size: 14px;">إظهار التفاصيل</a> &nbsp; --}}
+								<a class="btn btn-sm btn-info" href="{{ route('notice.deleteDetails', $member->IDTeam) }}" style="font-size: 14px;">إظهار التفاصيل</a> &nbsp;
 								<a class="btn btn-sm btn-success" href="" style="font-size: 14px;">تأكيد</a> &nbsp;
-								<a class="btn btn-sm btn-danger" href="" style="font-size: 14px;">تجاهل</a>
+								<a class="btn btn-sm btn-danger" href="{{ route('notice.destroyNotice', $member->id) }}" style="font-size: 14px;">تجاهل</a>
 							</div>
 						</div>
 						@endforeach
