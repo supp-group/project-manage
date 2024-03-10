c4df060ae25d920ca4a50d7440f86ada71385b32
ramaShahwan <rama692000@gmail.com>
2024-03-10 11:19:36 +0300

    update

--- app/Http/Controllers/TemporaryController.php
+++ app/Http/Controllers/TemporaryController.php
@@ -8,35 +8,34 @@ use App\Models\Qualification;
 use App\Models\Member;
 use Illuminate\Http\Request;
 use DateTime;
+
 class TemporaryController extends Controller
 {
-  public function getDeletedMember() 
-   {
-   $members = Temporary::where('operation','0')->
-   select('id','FullName','IDTeam','managerEmail')
-   ->orderBy('updated_at', 'desc')->paginate(4);
+  public function getDeletedMember()
+  {
+    $members = Temporary::where('operation', '0')->select('id', 'FullName', 'IDTeam', 'managerEmail')
+      ->orderBy('updated_at', 'desc')->paginate(4);
 
-   $paginationLinks = $members->withQueryString()->links('pagination::bootstrap-4');
-   return view('admin.notice.delete', [
-       'members' => $members,
-       'paginationLinks' => $paginationLinks
-   ]);
-   }
+    $paginationLinks = $members->withQueryString()->links('pagination::bootstrap-4');
+    return view('admin.notice.delete', [
+      'members' => $members,
+      'paginationLinks' => $paginationLinks
+    ]);
+  }
 
-   public function getEtidedMember() 
-   {
-   $members = Temporary::where('operation','1')->
-   select('id','FullName','IDTeam','managerEmail')
-   ->orderBy('updated_at', 'desc')->paginate(4);
+  public function getEtidedMember()
+  {
+    $members = Temporary::where('operation', '1')->select('id', 'FullName', 'IDTeam', 'managerEmail')
+      ->orderBy('updated_at', 'desc')->paginate(4);
 
-   $paginationLinks = $members->withQueryString()->links('pagination::bootstrap-4');
-   return view('admin.notice.edit', [
-       'members' => $members,
-       'paginationLinks' => $paginationLinks
-   ]);
-   }
-   public function storeUpdatedMember(Request $request)
-   {
+    $paginationLinks = $members->withQueryString()->links('pagination::bootstrap-4');
+    return view('admin.notice.edit', [
+      'members' => $members,
+      'paginationLinks' => $paginationLinks
+    ]);
+  }
+  public function storeUpdatedMember(Request $request)
+  {
     //    $validated = $request->validate([
     //        'NotPad' => 'required|max:255',
     //        'branch' => 'required',
@@ -61,144 +60,142 @@ class TemporaryController extends Controller
     //        'Image' =>'required',
     //    ]);
 
-       
-  // Convert Birthdate format
-  $birthDate = DateTime::createFromFormat('m/d/Y',$request->BirthDate);
-  $birthDateFormatted = $birthDate ? $birthDate->format('Y-m-d') : $request->BirthDate;
 
-  
-  $qualification = $request->Qualification;
+    // Convert Birthdate format
+    $birthDate = DateTime::createFromFormat('m/d/Y', $request->BirthDate);
+    $birthDateFormatted = $birthDate ? $birthDate->format('Y-m-d') : $request->BirthDate;
 
-  if (is_numeric($qualification)) {
+
+    $qualification = $request->Qualification;
+
+    if (is_numeric($qualification)) {
       // إذا كانت القيمة هي id
       $qualificationName = Qualification::where('id', $qualification)->value('Name');
-  } else {
+    } else {
       // إذا كانت القيمة هي اسم
       $qualificationName = $qualification;
-  }
-  
-$specialization = $request->Specialization;
+    }
 
-  if (is_numeric($specialization)) {
+    $specialization = $request->Specialization;
+
+    if (is_numeric($specialization)) {
       // إذا كانت القيمة هي id
       $specializationName = Qualification::where('id', $specialization)->value('specialization');
-
-  } else {
+    } else {
       // إذا كانت القيمة هي اسم
       $specializationName = $specialization;
-  }
-  $user = auth()->user();
+    }
+    $user = auth()->user();
 
-  $member = new Temporary(); 
-  $member->NotPad = $request->NotPad;
-  $member->branch = $request->branch;
-  $member->IDTeam  = $request->IDTeam;
-  $member->FullName = $request->FullName;
-  $member->MotherName = $request->MotherName;
-  $member->PlaceOfBirth = $request->PlaceOfBirth;
-  $member->BirthDate = $birthDateFormatted;
-  $member->Constraint = $request->Constraint;
-  $member->City = $request->City;
-  $member->IDNumber  = $request->IDNumber;
-  $member->Gender  = $request->Gender == 'ذكر' ? 'ذكر' : 'أنثى';
-  $member->Qualification = $qualificationName;
-  $member->Specialization  = $specializationName;
-  $member->Occupation  = $request->Occupation;
-  $member->MobilePhone  = $request->MobilePhone;
-  $member->HomeAddress  = $request->HomeAddress;
-  $member->WorkAddress  = $request->WorkAddress;
-  $member->HomePhone  = $request->HomePhone;
-  $member->WorkPhone  = $request->WorkPhone;
-  $member->DateOfJoin  = $request->DateOfJoin;
-  $member->operation ='1';
-  $member->managerEmail =$user->email;
-  $member->save();
+    $member = new Temporary();
+    $member->NotPad = $request->NotPad;
+    $member->branch = $request->branch;
+    $member->IDTeam  = $request->IDTeam;
+    $member->FullName = $request->FullName;
+    $member->MotherName = $request->MotherName;
+    $member->PlaceOfBirth = $request->PlaceOfBirth;
+    $member->BirthDate = $birthDateFormatted;
+    $member->Constraint = $request->Constraint;
+    $member->City = $request->City;
+    $member->IDNumber  = $request->IDNumber;
+    $member->Gender  = $request->Gender == 'ذكر' ? 'ذكر' : 'أنثى';
+    $member->Qualification = $qualificationName;
+    $member->Specialization  = $specializationName;
+    $member->Occupation  = $request->Occupation;
+    $member->MobilePhone  = $request->MobilePhone;
+    $member->HomeAddress  = $request->HomeAddress;
+    $member->WorkAddress  = $request->WorkAddress;
+    $member->HomePhone  = $request->HomePhone;
+    $member->WorkPhone  = $request->WorkPhone;
+    $member->DateOfJoin  = $request->DateOfJoin;
+    $member->operation = '1';
+    $member->managerEmail = $user->email;
+    $member->save();
 
-  // store image
-  if($request->hasfile('Image')){
+    // store image
+    if ($request->hasfile('Image')) {
       $img = $request->file('Image');
       $img_name = $img->getClientOriginalName();
       $img->move(public_path('images'), $img_name);
 
       Temporary::find($member->id)->update([
-      'Image'=> $img_name,
+        'Image' => $img_name,
       ]);
+    }
+
+    session()->flash('Edit', ' سيتم تعديل العضو بعد الموافقة عليه من قبل المدير');
+    return back();
   }
 
-  session()->flash('Edit', ' سيتم تعديل العضو بعد الموافقة عليه من قبل المدير');
-  return back();
-}
+  public function storeDeletedMember($id)
+  {
+    //  return $id;
 
-public function storeDeletedMember($id)
-   {
-  //  return $id;
-      
-  $user = auth()->user();
+    $user = auth()->user();
 
-  $Oldmember = Member::find($id);
-  //return   $Oldmember;
-  $member = new Temporary(); 
-  $member->NotPad = $Oldmember->NotPad;
-  $member->branch = $Oldmember->branch;
-  $member->IDTeam  = $Oldmember->IDTeam;
-  $member->FullName = $Oldmember->FullName;
-  $member->MotherName = $Oldmember->MotherName;
-  $member->PlaceOfBirth = $Oldmember->PlaceOfBirth;
-  $member->BirthDate = $Oldmember->BirthDate;
-  $member->Constraint = $Oldmember->Constraint;
-  $member->City = $Oldmember->City;
-  $member->IDNumber  = $Oldmember->IDNumber;
-  $member->Gender  = $Oldmember->Gender ;
-  $member->Qualification = $Oldmember->Qualification;
-  $member->Specialization  = $Oldmember->Specialization;
-  $member->Occupation  = $Oldmember->Occupation;
-  $member->MobilePhone  = $Oldmember->MobilePhone;
-  $member->HomeAddress  = $Oldmember->HomeAddress;
-  $member->WorkAddress  = $Oldmember->WorkAddress;
-  $member->HomePhone  = $Oldmember->HomePhone;
-  $member->WorkPhone  = $Oldmember->WorkPhone;
-  $member->DateOfJoin  = $Oldmember->DateOfJoin;
-  $member->Image  = $Oldmember->Image;
-  $member->operation ='0';
-  $member->managerEmail =$user->email;
-  $member->save();
+    $Oldmember = Member::find($id);
+    //return   $Oldmember;
+    $member = new Temporary();
+    $member->NotPad = $Oldmember->NotPad;
+    $member->branch = $Oldmember->branch;
+    $member->IDTeam  = $Oldmember->IDTeam;
+    $member->FullName = $Oldmember->FullName;
+    $member->MotherName = $Oldmember->MotherName;
+    $member->PlaceOfBirth = $Oldmember->PlaceOfBirth;
+    $member->BirthDate = $Oldmember->BirthDate;
+    $member->Constraint = $Oldmember->Constraint;
+    $member->City = $Oldmember->City;
+    $member->IDNumber  = $Oldmember->IDNumber;
+    $member->Gender  = $Oldmember->Gender;
+    $member->Qualification = $Oldmember->Qualification;
+    $member->Specialization  = $Oldmember->Specialization;
+    $member->Occupation  = $Oldmember->Occupation;
+    $member->MobilePhone  = $Oldmember->MobilePhone;
+    $member->HomeAddress  = $Oldmember->HomeAddress;
+    $member->WorkAddress  = $Oldmember->WorkAddress;
+    $member->HomePhone  = $Oldmember->HomePhone;
+    $member->WorkPhone  = $Oldmember->WorkPhone;
+    $member->DateOfJoin  = $Oldmember->DateOfJoin;
+    $member->Image  = $Oldmember->Image;
+    $member->operation = '0';
+    $member->managerEmail = $user->email;
+    $member->save();
 
-  // store image
-//   if($Oldmember->hasfile('Image')){
-//       $img = $Oldmember->file('Image');
-//       $img_name = $img->getClientOriginalName();
-//       $img->move(public_path('images'), $img_name);
+    // store image
+    //   if($Oldmember->hasfile('Image')){
+    //       $img = $Oldmember->file('Image');
+    //       $img_name = $img->getClientOriginalName();
+    //       $img->move(public_path('images'), $img_name);
 
-//       Temporary::find($member->id)->update([
-//       'Image'=> $img_name,
-//       ]);
-//   }
+    //       Temporary::find($member->id)->update([
+    //       'Image'=> $img_name,
+    //       ]);
+    //   }
 
-  session()->flash('delete', 'سيتم حذف العضو بعد الموافقة عليه من قبل المدير');
-  return back();
-}
+    session()->flash('delete', 'سيتم حذف العضو بعد الموافقة عليه من قبل المدير');
+    return back();
+  }
 
-public function destroyNotice($id)
-{
+  public function destroyNotice($id)
+  {
     Temporary::findOrFail($id)->delete();
 
-   session()->flash('delete', 'تم تجاهل الإشعار ');
-   return back(); 
-}
+    session()->flash('delete', 'تم تجاهل الإشعار ');
+    return back();
+  }
 
-public function editDetails($IDTeam)
-{
-   $memb = Temporary::where('IDTeam',$IDTeam)->first();
+  public function editDetails($IDTeam)
+  {
+    $memb = Temporary::where('IDTeam', $IDTeam)->first();
 
-  // return $memb;
-  return view('admin.notice.editDetails',['memb'=>$memb]);
-// return dd($mem);
-}
+    // return $memb;
+    return view('admin.notice.editDetails', ['memb' => $memb]);
+    // return dd($mem);
+  }
 
-public function deleteDetails($IDTeam)
-{
-   $member = Temporary::where('IDTeam',$IDTeam)->first();
-   return view('admin.notice.deleteDetails',compact('member'));
-}
-
+  public function deleteDetails($IDTeam)
+  {
+    $member = Temporary::where('IDTeam', $IDTeam)->first();
+    return view('admin.notice.deleteDetails', compact('member'));
+  }
 }
