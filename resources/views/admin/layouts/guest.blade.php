6a894a3a9d21408f12a1db9304188810b1b66b44
ramaShahwan <rama692000@gmail.com>
2024-03-14 14:21:33 +0300

    app

--- resources/views/admin/layouts/guest.blade.php
+++ resources/views/admin/layouts/guest.blade.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="ar">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        {{-- <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <meta property="og:title" content="الحزب الديمقراطي السوري">
+        <meta property="og:description" content=" ديمقراطية_عدالة_تنمية">
+        <meta property="og:image" content="{{URL::asset('assets/img/media/team.jpg')}}">
+
+        {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
