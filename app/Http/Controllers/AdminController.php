0ab4d5a360e74c846d08a2b7ecd5e831ba45400d
ramaShahwan <rama692000@gmail.com>
2024-03-14 11:57:53 +0300

    index

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,7 +13,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-            return  view('auth.login');
+          //  return  view('auth.login');
     
         // else if(auth()->user()->Role == "admin") {
         //     return view('admin.index');
